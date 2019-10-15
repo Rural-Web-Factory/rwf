4673a565c62720382395cf185d968e79752f79b6
Remy <remym16@outlook.fr>
2019-10-15 14:12:37 +0200

    exemple js

---
+++ exemple.js
@@ -0,0 +1,14 @@
+var test = 0;
+test = "coucou";
+
+const test = 0;
+
+var test = {
+    toto: {
+        casquette: "nike",
+        chaussure: "adidas"
+    },
+    tutu: 2
+}
+
+test.tutu.casquette
